34717036cb22ab1fd386bdb5e857acb21da241f7
Raphael Defosseux <raphael.defosseux@openairinterface.org>
2022-09-06 15:01:11 +0200

    fix(build): simplication and optimization of image building   * Remove un-necessary builder image packages   * Faster builds on built-from-source packages   * Support for Ubuntu20/22 image generation   * RHEL8-minimal target image   * health-check natively in image
    Signed-off-by: Raphael Defosseux <raphael.defosseux@openairinterface.org>

--- ci-scripts/flatten_image.py
+++ ci-scripts/flatten_image.py
@@ -36,7 +36,7 @@ def _parse_args() -> argparse.Namespace:
         '--tag', '-t',
         action='store',
         required=True,
-        help='Image Tag in image-name:image tag format',
+        help='Image Tag in image-name:image-tag format',
     )
     return parser.parse_args()
 
@@ -49,6 +49,9 @@ def perform_flattening(tag):
     if re.search('podman', podman_check.strip()):
         cli = 'sudo podman'
         image_prefix = 'localhost/'
+        # since HEALTHCHECK is not supported by podman import
+        # we don't flatten
+        return 0
     if cli == '':
         cmd = 'which docker || true'
         docker_check = subprocess.check_output(cmd, shell=True, universal_newlines=True)
@@ -74,6 +77,7 @@ def perform_flattening(tag):
     cmd += ' --change "WORKDIR /openair-pcf" '
     cmd += ' --change "EXPOSE 80/tcp" '
     cmd += ' --change "EXPOSE 9090/tcp" '
+    cmd += ' --change "HEALTHCHECK --interval=10s --timeout=15s --retries=6 CMD /openair-pcf/bin/healthcheck.sh" '
     cmd += ' --change "CMD [\\"/openair-pcf/bin/oai_pcf\\", \\"-c\\", \\"/openair-pcf/etc/pcf.conf\\", \\"-o\\"]" '
     cmd += ' --change "ENTRYPOINT [\\"/bin/bash\\", \\"/openair-pcf/bin/entrypoint.sh\\"]" '
     cmd += ' - ' + image_prefix + tag
